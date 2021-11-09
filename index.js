9997469488642cb8a9f235eb89ccfe5ce9f11b16
HoBocPhat <18520196@gm.uit.edu.vn>
2021-11-09 11:08:05 +0700

    change index

--- index.js
+++ index.js
@@ -19,16 +19,6 @@ import imgrerRouters from './routes/imgredients.js'
 
 dotenv.config();
 const app = express();
-// const CONNECTION_URL = process.env.URL_MONGODB_LOCAL;
-
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-// const PORT = process.env.PORT || 5000;
-// const dbOptions = {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true,
-//     //useCreateIndex: true,
-//     //useFindAndModify: false,
-// }
 
 const connectDB = async () => {
     try{
@@ -63,12 +53,4 @@ app.use('/save_post', savePostRoutes);
 app.use ('/reaction', reactionsRoutes);
 app.use('/role', roleRoutes);
 
-// mongoose.connect(CONNECTION_URL, dbOptions)
-//     .then(() => {
-//         app.listen(PORT, () => {
-//             console.log(`Server running on port ${PORT}`);
-//         })
-//     })
-//     .catch((error) => {
-//         console.log(error.message)
-// });
+
