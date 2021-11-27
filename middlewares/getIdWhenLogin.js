544fc03ed024d47f6752dc6d60cb6196914a8805
PhuongNguyen1202 <phuong322184@gmail.com>
2021-11-27 19:51:52 +0700

    update getId

--- middlewares/getIdWhenLogin.js
+++ middlewares/getIdWhenLogin.js
@@ -4,19 +4,18 @@ import User from '../models/User.js'
 export const getIdLogin = (req, res, next) => {
 
   const bearerHearder = req.headers['authorization'];
-  if (!bearerHearder) 
-    return res.status(403).send({ auth: false, message: 'No token provided.' });
 
-  const bearer = bearerHearder.split(' ');
-  const token = bearer[1];
-    if (token) 
-    {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, data) => {      
-            if (err) 
-              return res.status(500).send({ auth: false, message: err.message });    
-            req.userID = data.userID;
-            next()
-          });
+  if (bearerHearder) {
+    const bearer = bearerHearder.split(' ');
+    const token = bearer[1];
+    if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, data) => {
+        if (err)
+          return res.status(500).send({ auth: false, message: err.message });
+        req.userID = data.userID;
+        next()
+      });
     }
-    else next()
   }
+  else next()
+}
