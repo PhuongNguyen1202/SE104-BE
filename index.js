367cb2c782206388ac5b3cce6c022e9d10a1d318
PhuongNguyen1202 <phuong322184@gmail.com>
2021-11-29 20:44:14 +0700

    connect frontend

--- index.js
+++ index.js
@@ -35,14 +35,7 @@ const connectDB = async () => {
 
 connectDB()
 
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-// const dbOptions = {
-//         useNewUrlParser: true, 
-//         useUnifiedTopology: true,
-//         //useCreateIndex: true,
-//         //useFindAndModify: false,
-//     }
-
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 
 
 app.use(express.urlencoded({ extended: false}))
