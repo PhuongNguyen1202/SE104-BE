e9a443c4d4b8ad71694a09d43d3ff0d62c3bc9bc
HoBocPhat <18520196@gm.uit.edu.vn>
2021-11-30 19:59:55 +0700

    login gg fb

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
 'use strict';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken'
-
+import fetch from 'node-fetch';
+import {OAuth2Client} from 'google-auth-library';
 
 import User from '../models/User.js'
 import Role from '../models/Role.js'
+import { response } from 'express';
+
+const client = new OAuth2Client("811148561616-u5o162igd4bdqkb26lan40e7t356hh7f.apps.googleusercontent.com")
 //@route api/auth/register
 //@desc post registerform
 //@access public
@@ -54,7 +58,7 @@ export const registerUser = async(req, res) => {
 //@route api/auth/login
 //@desc post loginform
 //@access public
-export const login = async(req, res) => {
+export const login = async (req, res) => {
     const {email, password} = req.body
     if(!email || !password){
         return res.status(400).json({success: false, message: 'Missing field'})
@@ -131,43 +135,121 @@ export const addUser = async(req, res) => {
     }
 }
 
-//@route api/auth/forgot-password
-//@desc post email
+//@route api/auth/login-facebook
+//@desc post 
 //@access public
-// exports.forgotPassword = (req, res) => {
-//     const {email} = req.body
-    
-//     User.findOne({email}, (err, user) => {
-//         if(err || !user) {
-//             return res.status(400).json({success: false, message: "User with this email doesnot exist"})
+export const loginFacebook = async (req, res) => {
+    const {userID, accessToken} = req.body
+    let urlGraphFacebook = `https://graph.facebook.com/v12.0/${userID}/?fields=id,name,email&access_token=${accessToken}`
+    fetch(urlGraphFacebook, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(response => {
+        const {email, firstname, lastname, gender,password} = response
+        User.findOne({email}).exec(async (err, user) => {
+            if(err) {
+                return res.status(400).json({success: false, message: err.message})
+            } else{
+                if(user){
+                    const passwordValid = await bcrypt.compareSync(password, user.password)
 
-//         }
+                    if(!passwordValid){
+                        return res.status(400).json({success: false, message: 'Incorrect'})
+                    }
 
-//         const token = jwt.sign({_id: user._id}, process.env.RESET_PASSWORD_KEY)
+                    const accessToken = jwt.sign({userID: user._id.toString()}, process.env.ACCESS_TOKEN_SECRET)
+                    res.json({
+                        success: true,
+                        message: 'Loggin successfully',
+                        accessToken
+                    })
+                } else{
+                    const salt = await bcrypt.genSalt(10);
+                    const hashedPassword = await bcrypt.hash(password, salt);
+                    let temp = firstname.slice(0,1);
+                    const avatar = 'http://localhost:5000/avatar/default/' +`${temp}.jpg` 
 
-//         const data = {
-//             from: 'noreply@hello.com',
-//             to: email,
-//             subject: 'Reset Password Link',
-//             html: `
-//                 <h2> Nhấn vào link sau đây để reset password</h2>
-//                 <p>${process.env.CLIENT_URL}/auth/reset-password/${token}</p>
-//             `
-//         }
+                    const user = new User({email, firstname, lastname, password: hashedPassword, gender, avatar})
+                    const role = await Role.findOne({role_name: "user"})
+                    if(!role){
+                        return res.status(500).json({success: false, message: "Role is null"})
+                    }
+        
+                    user.role = role._id
+                    user.save((err) => {
+                    if (err) return res.status(500).json({success:false, message: err.message });
+                    });
 
-//         return user.updateOne({resetLink: token}, (err, success) => {
-//             if(err){
-//                 return res.status(400).json({success: false, message: err.message})
-//             } else {
-//                 mg.messages().send(data, function (error, body) {
-//                     if (error) {
-//                         return res.status(200).json({success: false, message: error.message })
-//                     }
-//                     return res.status(200).json({success: true, message: 'Email has been sent'})
-//                 });
-//             }
-//         })
-//     })
-// }
+                    const accessToken = jwt.sign({userID: user._id}, process.env.ACCESS_TOKEN_SECRET)
+                    res.json({
+                        success: true,
+                        message: 'User created successfully',
+                        accessToken
+                    })
+                }
+            }
+        })
+    })
+}
+
+//@route api/auth/login-google
+//@desc post
+//@access public
+export const googleLogin = async (req, res) => {
+    const {tokenId} = req.body
+
+    client.verifyIdToken({idToken: tokenId, audience: "811148561616-u5o162igd4bdqkb26lan40e7t356hh7f.apps.googleusercontent.com"}).then(async response => {
+        const {email_verified, firstname, lastname , email, gender, password} = response.payload
+        if(email_verified) {
+            User.findOne({email}).exec(async (err, user) => {
+                if(err){
+                return res.status(400).json({
+                    success: false,
+                    err: err.message
+                })
+            } else {
+                if(user){
+                    const passwordValid = await bcrypt.compareSync(password, user.password)
+
+                    if(!passwordValid){
+                        return res.status(400).json({success: false, message: 'Incorrect'})
+                    }
+
+                    const accessToken = jwt.sign({userID: user._id.toString()}, process.env.ACCESS_TOKEN_SECRET)
+                    res.json({
+                        success: true,
+                        message: 'Loggin successfully',
+                        accessToken
+                    }) 
+                } else {
+                    const salt = await bcrypt.genSalt(10);
+                    const hashedPassword = await bcrypt.hash(password, salt);
+                    let temp = firstname.slice(0,1);
+                    const avatar = 'http://localhost:5000/avatar/default/' +`${temp}.jpg` 
+
+                    const user = new User({email, firstname, lastname, password: hashedPassword, gender, avatar})
+                    const role = await Role.findOne({role_name: "user"})
+                    if(!role){
+                        return res.status(500).json({success: false, message: "Role is null"})
+                    }
+        
+                    user.role = role._id
+                    user.save((err) => {
+                    if (err) return res.status(500).json({success:false, message: err.message });
+                    });
+
+                    const accessToken = jwt.sign({userID: user._id}, process.env.ACCESS_TOKEN_SECRET)
+                    res.json({
+                        success: true,
+                        message: 'User created successfully',
+                        accessToken
+                    })
+                }
+            }
+            })
+        }
+    })
+}
 
 
