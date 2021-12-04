c0727163c12239504807b09167567e1b43c09ec7
unknown <lethiminhhienlight10@gmail.com>
2021-12-04 17:00:11 +0700

    updateSavePost_like

--- controllers/savePost.js
+++ controllers/savePost.js
@@ -18,6 +18,8 @@ export const saveToPost = async (req, res) => {
         if (!id_post) {
             return res.status(200).json({ message: "Not NULL" });
         }
+        if (!id_post.match(/^[0-9a-fA-F]{24}$/))//Kiểm tra kí tự
+            return res.status(200).json({ success: 0, message: "Post is not exist" });
         //Kiểm tra id_post có tồn tại không
         let check_id_post = await Post.findById(id_post);
         if (check_id_post) {
@@ -33,7 +35,7 @@ export const saveToPost = async (req, res) => {
                     save_post.list_post.push(id_post);
                 }
                 const data = await save_post.save();
-                console.log(data);
+                //console.log(data);
                 return res.status(200).json({ success: 1, message: "Saved success" });
             }
             else {//  chưa có savePost
@@ -43,13 +45,13 @@ export const saveToPost = async (req, res) => {
                         id_post
                     ]
                 });
-                console.log(id_post);
+                //console.log(id_post);
                 await newSavePost.save();
                 res.status(200).json({ success: 1, message: "Saved success" });
             }
         }
         else {//id_post không tồn tại
-            return res.status(200).json({ success: 0, message: "Post not exist!" })
+            return res.status(200).json({ success: 0, message: "Post is not exist!" })
         }
     } catch (error) {
         res.status(404).json({ message: error.message })
@@ -75,7 +77,7 @@ export const getAllPostInSavePost = async (req, res) => {
                 const data = [];
                 for (let temp of save_post.list_post) {
                     const post = await Post.findById(temp)//tìm bài post với id
-                    //console.log(temp1);
+                    //console.log(temp);
 
                     const InfoPost = {//thông tin cần lấy của mỗi bài post
                         "id_post": "",
@@ -84,6 +86,7 @@ export const getAllPostInSavePost = async (req, res) => {
                         "thumbnail_image": ""
                     }
                     const post_id_user = post.id_author;// lấy id_user của bài post lưu đó
+                    //console.log(post_id_user)
                     const user = await User.findById(post_id_user)
                     const author = user.lastname.concat(" ", user.firstname) //lấy tên tác giả
                     InfoPost.author = author;
@@ -109,12 +112,22 @@ export const deletePostInSavePost = async (req, res) => {
         console.log('DELETE A POST IN SAVEPOST');
         const id_post = req.body.id_post;
         const id_user = req.userID;
-        await savePost.findOneAndUpdate({ id_user }, {
-            $pull: {
-                list_post: id_post
-            }
-        }, { new: true });
-        res.status(200).json({ message: "Success" });
+        if (!id_post.match(/^[0-9a-fA-F]{24}$/))//Kiêm tra kí tự
+            return res.status(200).json({ success: 0, message: "Post is not exist" });
+        const checkpost = await Post.findById(id_post);
+        if (checkpost) {
+
+            await savePost.findOneAndUpdate({ id_user }, {
+                $pull: {
+                    list_post: id_post
+                }
+            }, { new: true });
+            res.status(200).json({ success: 1,message: "Success" });
+        }
+        else {//id_post không tồn tại
+            return res.status(200).json({ success: 0, message: "Post is not exist!" })
+
+        }
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
@@ -132,7 +145,7 @@ export const deleteManyPostInSavePost = async (req, res) => {
                 list_post: list_post
             }
         }, { new: true });
-        res.status(200).json({ message: "Success" });
+        res.status(200).json({ success: 1, message: "Success" });
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
