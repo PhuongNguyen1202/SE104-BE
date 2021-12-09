406e6927b3448c6d98e6eec0aff406f506bb2f6e
unknown <lethiminhhienlight10@gmail.com>
2021-12-09 10:59:55 +0700

    paginationSavePost

--- controllers/savePost.js
+++ controllers/savePost.js
@@ -5,7 +5,9 @@ import savePost from "../models/savePost.js";
 import Post from "../models/post.js";
 import User from "../models/User.js";
 
-
+//
+const LIMIT_OF_POST_DEFAULT = process.env.LIMIT_OF_POST_DEFAULT;
+const CURRENT_PAGE_DEFAULT = process.env.CURRENT_PAGE_DEFAULT;
 
 
 ///lưu một bài post
@@ -61,6 +63,25 @@ export const saveToPost = async (req, res) => {
 
 //Lấy danh sách list_post được lưu
 //http://localhost:5000/save_post
+function paginator(items, current_page, per_page_items) {
+    let page = current_page || 1,
+        per_page = per_page_items || 10,
+        offset = (page - 1) * per_page,
+
+        paginatedItems = items.slice(offset).slice(0, per_page_items),
+        total_pages = Math.ceil(items.length / per_page);
+
+    return {
+        page: page,
+        per_page: per_page,
+        pre_page: page - 1 ? page - 1 : null,
+        next_page: (total_pages > page) ? page + 1 : null,
+        total: items.length,
+        total_pages: total_pages,
+        data: paginatedItems
+    };
+}
+
 export const getAllPostInSavePost = async (req, res) => {
     try {
         console.log('GET ALL POST IN SAVE_POST');
@@ -78,7 +99,6 @@ export const getAllPostInSavePost = async (req, res) => {
                 for (let temp of save_post.list_post) {
                     const post = await Post.findById(temp)//tìm bài post với id
                     //console.log(temp);
-
                     const InfoPost = {//thông tin cần lấy của mỗi bài post
                         "id_post": "",
                         "title": "",
@@ -96,7 +116,74 @@ export const getAllPostInSavePost = async (req, res) => {
                     data.push(InfoPost);
                 }
                 //console.log(data);
-                res.status(200).json({ success: 1, data });
+                //pagination cho savePost
+                //check limit and page
+                const current_page = parseInt(req.query.page) || CURRENT_PAGE_DEFAULT;
+                const per_page = parseInt(req.query.limit) || LIMIT_OF_POST_DEFAULT;
+
+                if ((req.query.page && !Number.isFinite(parseInt(req.query.page))) || (req.query.limit && !Number.isFinite(parseInt(req.query.limit))))
+                    return res.status(200).json({ success: 0, message: "limit and page must be a Number" })
+
+                if ((req.query.page && parseInt(req.query.page) < 0) || (req.query.limit && parseInt(req.query.limit) < 0))
+                    return res.status(200).json({ success: 0, message: "limit and page must greater than 0" })
+                const saved_post = paginator(data, current_page, per_page)
+                res.status(200).json({ success: 1, result: saved_post, message: "Success" });
+            }
+        }
+
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const getSavedPostByUser = async (req, res) =>{
+    try {
+        console.log('GET ALL POST IN SAVE_POST OF ONE USER');
+        const id_user = req.params;
+        //console.log(id_user)
+        const save_post = await savePost.findOne(id_user)
+        //console.log(save_post)
+        if (!save_post) {//không có save_post
+            return res.status(400).json({ success: 0, message: 'savePost not exit' })
+        }
+        else {
+            if (save_post.list_post.length == 0) {
+                return res.status(400).json({ success: 0, message: 'no post saved' })
+            }
+            else {
+                const data = [];
+                for (let temp of save_post.list_post) {
+                    const post = await Post.findById(temp)//tìm bài post với id
+                    //console.log(temp);
+                    const InfoPost = {//thông tin cần lấy của mỗi bài post
+                        "id_post": "",
+                        "title": "",
+                        "author": "",
+                        "thumbnail_image": ""
+                    }
+                    const post_id_user = post.id_author;// lấy id_user của bài post lưu đó
+                    //console.log(post_id_user)
+                    const user = await User.findById(post_id_user)
+                    const author = user.lastname.concat(" ", user.firstname) //lấy tên tác giả
+                    InfoPost.author = author;
+                    InfoPost.id_post = temp;
+                    InfoPost.title = post.title; //lấy tiêu đề
+                    InfoPost.thumbnail_image = post.thumbnail_image;//lấy ảnh của bài post
+                    data.push(InfoPost);
+                }
+                //console.log(data);
+                //pagination cho savePost
+                //check limit and page
+                const current_page = parseInt(req.query.page) || CURRENT_PAGE_DEFAULT;
+                const per_page = parseInt(req.query.limit) || LIMIT_OF_POST_DEFAULT;
+
+                if ((req.query.page && !Number.isFinite(parseInt(req.query.page))) || (req.query.limit && !Number.isFinite(parseInt(req.query.limit))))
+                    return res.status(200).json({ success: 0, message: "limit and page must be a Number" })
+
+                if ((req.query.page && parseInt(req.query.page) < 0) || (req.query.limit && parseInt(req.query.limit) < 0))
+                    return res.status(200).json({ success: 0, message: "limit and page must greater than 0" })
+                const saved_post = paginator(data, current_page, per_page)
+                res.status(200).json({ success: 1, result: saved_post, message: "Success" });
             }
         }
 
@@ -122,7 +209,7 @@ export const deletePostInSavePost = async (req, res) => {
                     list_post: id_post
                 }
             }, { new: true });
-            res.status(200).json({ success: 1,message: "Success" });
+            res.status(200).json({ success: 1, message: "Success" });
         }
         else {//id_post không tồn tại
             return res.status(200).json({ success: 0, message: "Post is not exist!" })
--- routes/savePost.js
+++ routes/savePost.js
@@ -4,15 +4,19 @@ import {
     deleteManyPostInSavePost,
     deletePostInSavePost, 
     getAllPostInSavePost,  
+    getSavedPostByUser,  
     saveToPost } 
     from '../controllers/savePost.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 import {isUser} from '../middlewares/isUser.js';
+import { isAdmin } from '../middlewares/isAdmin.js';
+
 
 const router = express.Router();
 
 router.post('/saved', [verifyToken, isUser], saveToPost);
 router.get('/', [verifyToken, isUser], getAllPostInSavePost);
+router.get('/savedPostUser/:id_user',[verifyToken, isAdmin], getSavedPostByUser);
 router.post('/unsaved', [verifyToken, isUser], deletePostInSavePost);
 router.post('/many_unsaved', [verifyToken, isUser], deleteManyPostInSavePost);
 router.delete('/all_unsaved', [verifyToken, isUser], deleteAll_ListPostInSavePost);
